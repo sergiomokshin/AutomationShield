6cd39a289f2db0ac2c2fb2aaf2874defceb2c358
Sergio Mokshin <sergio.mokshin@gmail.com>
2015-07-22 17:45:10 -0300

    DES

--- Bluetooth/Arduino/TestBlueToothLCD/TestBlueToothLCD.ino
+++ Bluetooth/Arduino/TestBlueToothLCD/TestBlueToothLCD.ino
@@ -47,7 +47,16 @@ keypad()                        Read a value from the keypad. Returns 0 if no ke
 #include "Wire.h"
 #include <LiquidCrystal_I2C.h>	// For the LCD
 
-LiquidCrystal_I2C lcd(0x27,16,2);// set the LCD address to 0x27 for a 16 chars and 2 line display
+//Endereços de LCDs 1602
+LiquidCrystal_I2C lcd(0x3F, 16,2);// set the LCD address to 0x27 for a 16 chars and 2 line display
+//LiquidCrystal_I2C lcd(0x27, 16,2);// set the LCD address to 0x27 for a 16 chars and 2 line display
+//LiquidCrystal_I2C lcd(32,16,2); // set the LCD address to 0x20 for a 16 chars and 2 line display
+
+//Endereços de LCDs 2004
+//LiquidCrystal_I2C lcd(0x27, 20, 4); // set the LCD address to 0x27 for a 20 chars and 4 line display
+
+//Se necessário utilizar um I2C Scanner http://playground.arduino.cc/Main/I2cScanner
+
 #define DS1307_I2C_ADDRESS 0x68
 
 OneWire ds(2);  // pin 2 (with 4.7K resistor)
